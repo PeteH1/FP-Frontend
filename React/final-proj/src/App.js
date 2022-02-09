f2bdc578d9116d94f514bd30e4fd4746828020cd
JBIAh <j.b.iahunanya@outlook.com>
2022-02-09 15:47:11 +0000

    Local commit. Changed css folder to styling

--- React/final-proj/src/App.js
+++ React/final-proj/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import './css/App.css';
+import './styling/App.css';
 import FPNav from './components/FP-Nav';
 import Home from './components/Home';
 import Footer from './components/Footer';
--- React/final-proj/src/index.js
+++ React/final-proj/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
+import './styling/index.css';
 import App from './App';
 import reportWebVitals from './_tests/reportWebVitals';
 
